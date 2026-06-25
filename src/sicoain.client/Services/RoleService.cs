35977ef22b2b9c9f51769e2c6142a3a800e7eba8
Alex Coronell <alexcoronell@gmail.com>
2026-06-24 19:17:06 -0500

    fix(client): correct RoleService endpoint to match API controller route

--- src/sicoain.client/Services/RoleService.cs
+++ src/sicoain.client/Services/RoleService.cs
@@ -5,7 +5,7 @@ namespace sicoain.client.Services
 {
     public class RoleService : BaseService<RoleDto, CreateRoleRequest, UpdateRoleRequest>, IRoleService
     {
-        public RoleService(HttpClient httpClient) : base(httpClient, "role")
+        public RoleService(HttpClient httpClient) : base(httpClient, "Roles")
         {
 
         }
